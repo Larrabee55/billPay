06f2610779960be81856bfa6ad5aea328fa4ac15
cwp132 <44931480+cwp132@users.noreply.github.com>
2019-05-27 15:59:49 -0600

    db/table

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,41 @@
-CREATE DATABASE billPayDb;
-USE billPayDb;
+CREATE DATABASE billPay_db;
+USE billPay_db;
 
-CREATE TABLE billPayDb
-(
-  id int NOT NULL
-  AUTO_INCREMENT,
-  bill_name varchar
-  (255) NOT NULL,
-  amount DECIMAL
-  (10,2) NOT NULL,
-  due_date DATE NOT NULL,
-  PRIMARY KEY
-  (id)
-  );
+
+
+
+create table userIou(
+id integer auto_increment not null,
+recipient varchar(100) not null,
+amount int(100) not null,
+memo varchar(200),
+userId int(100),
+primary key(id)
+);
+
+create table receipt(
+id int auto_increment not null,
+catagory varchar(200) not null,
+amount int not null,
+dateBought date,
+userId int(100),
+primary key(id)
+);
+
+create table userBill(
+id int auto_increment not null,
+catagory varchar(200) not null,
+bill_name varchar(200) not null,
+amount int(100) not null,
+dueDate date,
+userId int(100),
+primary key(id) 
+);
+
+create table userInfo(
+id int auto_increment not null,
+username varchar(200) not null,
+userId int(100),
+password varchar(200) not null,
+primary key(id)
+);
